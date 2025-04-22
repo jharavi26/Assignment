a9ec78967d2b2d0bff4b1e0f669e44cf501b9203
Ravikumar Jha <jharavi153@gmail.com>
2025-04-23 01:11:47 +0530

    Setup

---
+++ E-Commerce/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-center text-gray-800 mt-4 underline'>Ravikumar </h1>
+      
+    </div>
+  )
+}
+
+export default App
